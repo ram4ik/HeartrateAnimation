3dcf25bf632b95e44be6bafbdf8737f01a5e9e85
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-05 15:53:38 +0300

    Initial Commit

---
+++ HeartrateAnimation/HeartrateAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartrateAnimationApp.swift
+//  HeartrateAnimation
+//
+//  Created by ramil on 05.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct HeartrateAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
